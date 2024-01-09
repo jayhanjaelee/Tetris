68f55d2b7e7c97c725b34b78800f520db9cf5c46
jayhanjaelee <jayhanjaelee@gmail.com>
2024-01-09 22:57:19 +0900

    Add UI.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <raylib.h>
 #include "game.h"
+#include "colors.h"
 
 double lastUpdateTime = 0;
 
@@ -16,23 +17,32 @@ bool EventTriggered(double interval)
 
 int main()
 {
-    Color darkBlue = { 44, 44, 127, 255 };
-    InitWindow(300, 600, "raylib Tetris");
+    InitWindow(500, 620, "raylib Tetris");
     SetTargetFPS(60);
+
+    Font font = LoadFontEx("Font/monogram.ttf", 64, 0, 0);
 
     Game game = Game();
 
     while(WindowShouldClose() == false)
     {
         game.HandleInput();
-        if (EventTriggered(0.02))
+        if (EventTriggered(0.2))
         {
             game.MoveBlockDown();
         }
         BeginDrawing();
-        game.Draw();
         ClearBackground(darkBlue);
+        DrawTextEx(font, "Score", {365, 15}, 38, 2, WHITE); 
+        DrawTextEx(font, "Next", {370, 175}, 38, 2, WHITE); 
+        if (game.gameOver)
+        {
+            DrawTextEx(font, "GAME OVER", {320, 450}, 38, 2, WHITE);
+        }
 
+        DrawRectangleRounded({320, 55, 170, 60}, 0.3, 6, lightBlue);
+        DrawRectangleRounded({320, 215, 170, 180}, 0.3, 6, lightBlue);
+        game.Draw();
         EndDrawing();
     }
 
