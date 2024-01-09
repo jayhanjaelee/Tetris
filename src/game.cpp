d6d2e6f3ca01a862c962cf6bf52aa220861c9f24
jayhanjaelee <jayhanjaelee@gmail.com>
2024-01-09 15:06:44 +0900

    Add Rotate feature.

--- src/game.cpp
+++ src/game.cpp
@@ -47,6 +47,9 @@ void Game::HandleInput()
         case KEY_DOWN:
         MoveBlockDown();
         break;
+        case KEY_UP:
+        RotateBlock();
+        break;
     }
 }
 
@@ -88,4 +91,13 @@ bool Game::IsBlockOutside()
         }
     }
     return false;
+}
+
+void Game::RotateBlock()
+{
+    currentBlock.Rotate();
+    if (IsBlockOutside())
+    {
+        currentBlock.UndoRotation();
+    }
 }
